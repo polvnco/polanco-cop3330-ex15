ec31f7ffe2b2a0386e1b39d59a2367d0dcbcbb64
polo <cpolanco000@gmail.com>
2021-07-25 19:18:48 -0400

    pom.xml

--- src/main/java/Assignment1/example/ex15.java
+++ src/main/java/Assignment1/example/ex15.java
@@ -6,19 +6,16 @@ package Assignment1.example;
 
 import java.util.Scanner;
 
-public class ex15
-{
-    public static void main( String[] args )
-    {
-        System.out.print( "What is the password? " );
+public class ex15 {
+    public static void main(String[] args) {
+        System.out.print("What is the password? ");
         Scanner pw = new Scanner(System.in);
         String password = "abc$123";
         String masterPassword = pw.nextLine();
 
-        if (!masterPassword.equals(password) ){
+        if (!masterPassword.equals(password)) {
             System.out.println("I don't know you.");
-        }
-        else {
+        } else {
             System.out.println("Welcome!");
         }
 
